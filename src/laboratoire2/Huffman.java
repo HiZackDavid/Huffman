eb8378f680b2d324395d488fa5fbff107d5c886f
Isaac David Zolana <HiZackDavid@hotmail.com>
2023-02-13 16:57:17 -0500

    Add Node class Begin working on the creation of a huffman tree
    Co-authored-by: JP <Jo-pl@users.noreply.github.com>

--- src/laboratoire2/Huffman.java
+++ src/laboratoire2/Huffman.java
@@ -7,97 +7,129 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 
 public class Huffman {
-	public static final int END_OF_FILE = -1;
 
-	public void Compresser(String nomFichierEntre, String nomFichierSortie) {
-	}
+  public static final int END_OF_FILE = -1;
 
-	public void Decompresser(String nomFichierEntre, String nomFichierSortie) {
-	}
+  public void Compresser(String nomFichierEntre, String nomFichierSortie) {}
 
-	/**
-	 * <p>
-	 * Creates a frequency table.
-	 * </p>
-	 * <p>
-	 * It will read a file and create a table containing the number of <br>
-	 * occurrences of each character in it.
-	 * </p>
-	 * 
-	 * @param filePath The location of the file.
-	 * @throws FileNotFoundException Thrown if the file is not found.
-	 */
-	public LinkedHashMap<Character, Integer> createFrequencyTable(String filePath) throws FileNotFoundException {
-		LinkedHashMap<Character, Integer> frequency = new LinkedHashMap<>();
-		File file = new File(filePath);
-		FileInputStream reader = new FileInputStream(file);
-		BufferedInputStream bis = null;
-		int nbBytes = 0;
+  public void Decompresser(String nomFichierEntre, String nomFichierSortie) {}
 
-		try {
-			int singleCharInt;
-			char singleChar;
-			bis = new BufferedInputStream(reader);
+  /**
+   * <p>
+   * Creates a frequency table.
+   * </p>
+   * <p>
+   * It will read a file and create a table containing the number of <br>
+   * occurrences of each character in it.
+   * </p>
+   *
+   * @param filePath The location of the file.
+   * @throws FileNotFoundException Thrown if the file is not found.
+   */
+  public LinkedHashMap<Character, Integer> createFrequencyTable(
+    String filePath
+  ) throws FileNotFoundException {
+    LinkedHashMap<Character, Integer> frequency = new LinkedHashMap<>();
+    File file = new File(filePath);
+    FileInputStream reader = new FileInputStream(file);
+    BufferedInputStream bis = null;
+    //int nbBytes = 0;
 
-			while ((singleCharInt = bis.read()) != END_OF_FILE) {
-				singleChar = (char) singleCharInt;
-				System.out.println(String.format("0x%X %c", singleCharInt, singleChar));
-				nbBytes++;
-				if (!frequency.containsKey(singleChar)) {
-					frequency.put(singleChar, 1);
-				} else {
-					frequency.put(singleChar, frequency.get(singleChar) + 1);
-				}
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				bis.close();
-				reader.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-		System.out.println("number of bytes read in binary mode : " + nbBytes);
-		System.out.println(frequency.entrySet());
+    try {
+      int singleCharInt;
+      char singleChar;
+      bis = new BufferedInputStream(reader);
 
-		return sortFrequencyTable(frequency);
-	}
+      while ((singleCharInt = bis.read()) != END_OF_FILE) {
+        singleChar = (char) singleCharInt;
+        //System.out.println(String.format("0x%X %c", singleCharInt, singleChar));
+        //nbBytes++;
+        if (!frequency.containsKey(singleChar)) {
+          frequency.put(singleChar, 1);
+        } else {
+          frequency.put(singleChar, frequency.get(singleChar) + 1);
+        }
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    } finally {
+      try {
+        bis.close();
+        reader.close();
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    }
+    //System.out.println("number of bytes read in binary mode : " + nbBytes);
 
-	/**
-	 * <p>
-	 * Sorts a frequency table in ascending order of it's value.
-	 * </p>
-	 * 
-	 * @param frequencyTable The frequency table to sort.
-	 * @return A sorted frequency table.
-	 */
-	private LinkedHashMap<Character, Integer> sortFrequencyTable(LinkedHashMap<Character, Integer> frequencyTable) {
-		LinkedHashMap<Character, Integer> sortedFrequencyTable = new LinkedHashMap<>();
-		List<Map.Entry<Character, Integer>> entries = new ArrayList<>(frequencyTable.entrySet());
-		
-		Collections.sort(entries, new FrequencyComparator());
+    return sortFrequencyTable(frequency);
+  }
 
-		for (Map.Entry<Character, Integer> entry : entries) {
-			sortedFrequencyTable.put(entry.getKey(), entry.getValue());
-		}
+  /**
+   * <p>
+   * Sorts a frequency table in ascending order of it's value.
+   * </p>
+   *
+   * @param frequencyTable The frequency table to sort.
+   * @return A sorted frequency table.
+   */
+  private LinkedHashMap<Character, Integer> sortFrequencyTable(
+    LinkedHashMap<Character, Integer> frequencyTable
+  ) {
+    LinkedHashMap<Character, Integer> sortedFrequencyTable = new LinkedHashMap<>();
+    List<Map.Entry<Character, Integer>> entries = new ArrayList<>(
+      frequencyTable.entrySet()
+    );
 
-		return sortedFrequencyTable;
-	}
+    Collections.sort(entries, new FrequencyComparator());
 
-	public static void main(String[] args) {
-		Huffman huff = new Huffman();
-		try {
-			LinkedHashMap<Character, Integer> frequency = huff.createFrequencyTable("src/laboratoire2/Temp.txt");
-			System.out.println(frequency.entrySet());
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
-	}
+    for (Map.Entry<Character, Integer> entry : entries) {
+      sortedFrequencyTable.put(entry.getKey(), entry.getValue());
+    }
+
+    return sortedFrequencyTable;
+  }
+
+  public void createHuffmanTree(Map<Character, Integer> frequencyTable) {
+    // TODO: Extraire les deux éléments ayant les fréquences les plus faibles
+    Iterator<Entry<Character, Integer>> itr = frequencyTable
+      .entrySet()
+      .iterator();
+    int loop = 0;
+    while (itr.hasNext() && loop <= 1) {
+      Object tmp = itr.next();
+      System.out.println(tmp);
+      loop++;
+    }
+  }
+
+  public Object combine(Map<Character, Integer> frequencyTable) {
+    return null;
+  }
+
+  public static void main(String[] args) {
+    Huffman huff = new Huffman();
+    try {
+      LinkedHashMap<Character, Integer> frequency = huff.createFrequencyTable(
+        "src/laboratoire2/Temp.txt"
+      );
+
+      huff.createHuffmanTree(frequency);
+      // Formatting
+      frequency.forEach((key, val) -> {
+        String str = ("" + key + " : " + val);
+        str = str.replace(System.lineSeparator(), "HEY");
+        System.out.println(str);
+      });
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
+    }
+  }
 }
---
+++ src/laboratoire2/Node.java
@@ -0,0 +1,56 @@
+package laboratoire2;
+
+public class Node {
+
+  private char key;
+  private int value;
+  private Node leftChild;
+  private Node rightChild;
+
+  Node(char key, int value) {
+    this.key = key;
+    this.value = value;
+    this.rightChild = null;
+    this.leftChild = null;
+  }
+
+  Node(int value) {
+    this.value = value;
+    this.rightChild = null;
+    this.leftChild = null;
+  }
+
+  public char getKey() {
+    return this.key;
+  }
+
+  public int getValue() {
+    return this.value;
+  }
+
+  public void setKey(char key) {
+    this.key = key;
+  }
+
+  public void setValue(int value) {
+    this.value = value;
+  }
+
+  public Node getLeftChild() {
+    return this.leftChild;
+  }
+
+  public void setLeftChild(Node leftChild) {
+    this.leftChild = leftChild;
+  }
+
+  public Node getRightChild() {
+    return this.rightChild;
+  }
+
+  public void setRightChild(Node rightChild) {
+    this.rightChild = rightChild;
+  }
+
+  public void add(int value) {}
+}
